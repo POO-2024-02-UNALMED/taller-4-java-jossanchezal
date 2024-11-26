4f62453e66b23af645143aae59e655b49214b381
jossanchezal <jossanchezal@unal.edu.co>
2024-11-26 13:08:26 -0500

    5

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -5,7 +5,7 @@ public class Grupo {
     public Persona[] estudiantes;
     public Persona profesor;
     public Asignatura asignatura;
-    public final int codigo; /* Se quito la asignacion del final, para asignarlo en el constructor */
+    public final int codigo; 
     public String horario;
 
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
@@ -40,3 +40,5 @@ public class Grupo {
     void cambiarEstudiante(int indice, Persona estudiante) {
         estudiantes[indice] = estudiante;
     }
+    }
+
